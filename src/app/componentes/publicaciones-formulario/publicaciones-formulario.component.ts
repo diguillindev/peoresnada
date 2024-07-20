7fd14f2077078c690a84f64a378da041ebba9105
diguillindev <scontreras.dev@gmail.com>
2024-07-20 16:12:21 -0400

    validaciones

--- src/app/componentes/publicaciones-formulario/publicaciones-formulario.component.ts
+++ src/app/componentes/publicaciones-formulario/publicaciones-formulario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonInput, IonItem, IonButton, IonIcon, IonText, IonList } from "@ionic/angular/standalone";
+import { IonInput, IonItem, IonButton, IonIcon, IonText, IonList, IonModal, IonHeader, IonTitle, IonButtons, IonContent } from "@ionic/angular/standalone";
 import { cameraOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { PublicacionesService } from 'src/app/servicios/publicaciones.service';
@@ -11,12 +11,14 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './publicaciones-formulario.component.html',
   styleUrls: ['./publicaciones-formulario.component.scss'],
   standalone: true,
-  imports:[IonInput, IonItem, IonButton, IonIcon, IonText, IonList, FormsModule]
+  imports:[IonInput, IonItem, IonButton, IonIcon, IonText, IonList, FormsModule, IonModal, IonHeader, IonTitle, IonButtons, IonContent]
 })
 export class PublicacionesFormularioComponent  implements OnInit {
 
   titulo: string = '';
   descripcion: string = '';
+  
+
 
   constructor(private publicacionesService: PublicacionesService) {
 
@@ -34,5 +36,7 @@ export class PublicacionesFormularioComponent  implements OnInit {
     console.log('Titulo:', this.titulo);
     console.log('Descripcion:', this.descripcion);
   }
+
+ 
 
 }
