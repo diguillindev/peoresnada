d5b63091d1fedf20e1fb536a39bf06db535e6ebe
diguillindev <scontreras.dev@gmail.com>
2024-07-22 11:43:26 -0400

    modal configurado con directiva ngif

--- src/app/paginas/lista/lista.page.ts
+++ src/app/paginas/lista/lista.page.ts
@@ -1,40 +1,8 @@
-/*import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonIcon, IonFab } from '@ionic/angular/standalone';
-import { addOutline } from 'ionicons/icons';
-import { addIcons } from 'ionicons';
-import { PublicacionesListaComponent } from "../../componentes/publicaciones-lista/publicaciones-lista.component";
-
-
-@Component({
-  selector: 'app-lista',
-  templateUrl: './lista.page.html',
-  styleUrls: ['./lista.page.scss'],
-  standalone: true,
-  imports: [IonFab, RouterLink, IonIcon, IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, PublicacionesListaComponent]
-})
-export class ListaPage implements OnInit {
-
-  constructor() { 
-   
-    addIcons({
-      addOutline
-     });
-
-  }
-
-  ngOnInit() {
-  }
-
-}*/
-// src/app/pages/lista/lista.page.ts
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonIcon, IonFab, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons, IonIcon, IonFab, IonList, IonItem, IonLabel, IonModal } from '@ionic/angular/standalone';
 import { addOutline, trashOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { PublicacionesListaComponent } from "../../componentes/publicaciones-lista/publicaciones-lista.component";
@@ -46,16 +14,21 @@ import { postUsuario } from 'src/app/modelo/postusuario';
   templateUrl: './lista.page.html',
   styleUrls: ['./lista.page.scss'],
   standalone: true,
-  imports: [IonFab, RouterLink, IonIcon, IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, PublicacionesListaComponent, IonList, IonItem, IonLabel]
+  imports: [IonModal, IonFab, RouterLink, IonIcon, IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, PublicacionesListaComponent, IonList, IonItem, IonLabel]
 })
 export class ListaPage implements OnInit {
+
+
   publicaciones: postUsuario[] = [];
+
+  isModalOpen = false;
+  currentPublicacionId: number | null = null;
 
   constructor(private publicacionesService: PublicacionesService) {
     addIcons({
       addOutline,
       trashOutline
-      
+
     });
   }
 
@@ -63,9 +36,27 @@ export class ListaPage implements OnInit {
     this.publicaciones = this.publicacionesService.getPostUsuarios();
   }
 
-  eliminarPublicacion(id: number) {
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  openModal(id: number) {
+    this.currentPublicacionId = id;
+    this.setOpen(true);
+  }
+
+  /*eliminarPublicacion(id: number) {
     this.publicacionesService.eliminarPostUsuario(id);
     this.publicaciones = this.publicacionesService.getPostUsuarios(); // Refresh the list
+  }*/
+
+  eliminarPublicacion(id: number) {
+    if (id !== null) {
+      this.publicacionesService.eliminarPostUsuario(id);
+      this.publicaciones = this.publicacionesService.getPostUsuarios();
+      this.setOpen(false);
+    }
   }
+
 }
 
