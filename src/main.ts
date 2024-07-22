0116be10ba2c899b11a5b0c9e280f0e32da1e428
diguillindev <scontreras.dev@gmail.com>
2024-07-22 19:24:08 -0400

    uso de camara web

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
+
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+// Call the element loader before the render call
+defineCustomElements(window);
+
 
 bootstrapApplication(AppComponent, {
   providers: [
